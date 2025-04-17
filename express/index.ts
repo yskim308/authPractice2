5602f305f96db66c65f17810fb17c6b9a44ea229
Young Seo Kim <youngseokim7@gmail.com>
2025-04-17 14:51:42 +0900

    use user seessions once authenticated

--- express/index.ts
+++ express/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
 import passport from "passport";
+import session from "express-session";
 import "./auth/googleAuth.ts";
 import { router as authRouter } from "./routes/authRoutes.ts";
 
 const app = express();
 app.use(cors());
 const port = 4000;
+
+app.use(session({ secret: "cats" }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/protected", (req: express.Request, res: express.Response) => {
   res.send("hello from protected");
