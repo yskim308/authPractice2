c796f780a6d96c68d4a7d7f38fe111194b41467d
Young Seo Kim <youngseokim7@gmail.com>
2025-04-17 14:17:59 +0900

    setup express app

--- express/index.ts
+++ express/index.ts
@@ -2,3 +2,11 @@ import express from "express";
 import cors from "cors";
 import passport from "passport";
 
+const app = express();
+app.use(cors());
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
