53bf1de2a894406742b72fa1f8898ceb0c96f8b7
Young Seo Kim <youngseokim7@gmail.com>
2025-04-17 14:42:19 +0900

    configure auth and routes with express and passport

--- express/index.ts
+++ express/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import cors from "cors";
 import passport from "passport";
+import "./auth/googleAuth.ts";
+import { router as authRouter } from "./routes/authRoutes.ts";
 
 const app = express();
 app.use(cors());
 const port = 4000;
+
+app.get("/protected", (req: express.Request, res: express.Response) => {
+  res.send("hello from protected");
+});
+
+app.get("/auth/google", authRouter);
+app.get("/auth/google/callback", authRouter);
+app.get("/auth/failure", authRouter);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
---
+++ express/routes/authRoutes.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import passport from "passport";
+export { router };
+
+const router = express.Router();
+
+router.get("/auth/google", (req: express.Request, res: express.Response) => {
+  passport.authenticate("google", { scope: ["email", "profile"] });
+});
+
+router.get(
+  "/auth/google/callback",
+  (req: express.Request, res: express.Response) => {
+    passport.authenticate("google", {
+      successRedirect: "/protected",
+      failureRedirect: "/auth/failure",
+    });
+  },
+);
+
+router.get("/auth/failure", (req: express.Request, res: express.Response) => {
+  res.status(401).send("auth failure");
+});
