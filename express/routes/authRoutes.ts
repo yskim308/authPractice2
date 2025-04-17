ead325fab542cf44a0bf82af55c3e59a1a6c9bc2
Young Seo Kim <youngseokim7@gmail.com>
2025-04-17 19:07:46 +0900

    fix passport to be middleware for routes

--- express/routes/authRoutes.ts
+++ express/routes/authRoutes.ts
@@ -5,6 +5,19 @@ export { router };
 
 const router = express.Router();
 
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["email", "profile"] }),
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/protected",
+    failureRedirect: "/auth/failure",
+  }),
+);
+
 router.get("/google", (req: express.Request, res: express.Response) => {
   passport.authenticate("google", { scope: ["email", "profile"] });
 });
